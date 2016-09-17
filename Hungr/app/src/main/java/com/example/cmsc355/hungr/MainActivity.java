08d34d536da877c32b113e780bb2dc0adf4c9e49
Hagoodkk <Hagood.Kyle@gmail.com>
2016-09-17 16:41:36 -0400

    this

--- Hungr/app/src/main/java/com/example/cmsc355/hungr/MainActivity.java
+++ Hungr/app/src/main/java/com/example/cmsc355/hungr/MainActivity.java
@@ -5,6 +5,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import org.json.JSONObject;
+import org.json.JSONArray;
+import org.json.simple.parser.JSONParser;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -32,9 +34,13 @@ public class MainActivity extends AppCompatActivity {
                     "FtZpcHv3NFy3gKd-XxvvSDmN3Gk");
             apiResponse = apiCall.searchForBusinessesByLocation("food", "Richmond, VA");
             try{
-                JSONObject apiR = new JSONObject(apiResponse);
+                JSONObject apiJSON = new JSONObject(apiResponse);
+                JSONArray businessArray = apiJSON.getJSONArray("businesses");
+                System.out.println(businessArray.toString(4));
+                JSONObject firstBiz = businessArray.getJSONObject(0);
+                System.out.println(firstBiz.toString(4));
 
-                System.out.println(apiR);
+
             }
             catch(Exception e){
                 System.out.println("Fail 1");
