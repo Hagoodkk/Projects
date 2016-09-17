06946a0aa125cfe878026593210f9493fdaf7991
Hagoodkk <Hagood.Kyle@gmail.com>
2016-09-17 15:17:29 -0400

    This Push

--- Hungr/app/src/main/java/com/example/cmsc355/hungr/MainActivity.java
+++ Hungr/app/src/main/java/com/example/cmsc355/hungr/MainActivity.java
@@ -4,9 +4,9 @@ import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import org.json.JSONObject;
+
 public class MainActivity extends AppCompatActivity {
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,19 +17,28 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onStart() {
         super.onStart();
-        pingYelpAPI instance = new pingYelpAPI();
-        instance.execute();
+        pingYelpAPI apiResponse = new pingYelpAPI();
+        apiResponse.execute();
     }
 
     private class pingYelpAPI extends AsyncTask<String, String, String> {
 
         @Override
         protected String doInBackground(String... params) {
+            String apiResponse;
             YelpAPI apiCall = new YelpAPI("4gMlTiNc9lXpqP9YIwmbJg",
                     "yKuJ3AV6pkw4ITB4WBfHuZq-JL0",
                     "Eeiax8f5iIYnh6IMWmHLVTKYWSGATfr1",
                     "FtZpcHv3NFy3gKd-XxvvSDmN3Gk");
-            System.out.println(apiCall.searchForBusinessesByLocation("food", "Richmond, VA"));
+            apiResponse = apiCall.searchForBusinessesByLocation("food", "Richmond, VA");
+            try{
+                JSONObject apiR = new JSONObject(apiResponse);
+                System.out.println(apiR);
+            }
+            catch(Exception e){
+                System.out.println("Fail 1");
+            }
+
             return null;
         }
     }
